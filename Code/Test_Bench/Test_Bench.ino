d77464deb1ddf68fde2b86ff0666f36032204672
AIM75 <ahmedibrahimmohamed75@gmail.com>
2024-04-18 04:18:48 +0200

    Now you can control thrust using Serial monitor and potentiometer with safety measures

--- Code/Test_Bench/Test_Bench.ino
+++ Code/Test_Bench/Test_Bench.ino
@@ -1,3 +1,6 @@
+/*
+Initial configuration must be done while connecting to the arduino
+*/
 #include <HX711_ADC.h>
 #include <Servo.h>
 
@@ -7,20 +10,26 @@
 #define ESC_OUT 9
 
 HX711_ADC LoadCell(HX_DOUT, HX_SCK);
+Servo motor;
 
-float Calibration_Factor = 0;
+float Calibration_Factor = 450;  //Can not be equal to zero Nan
 float Thrust = 0;
 int Throttle = 0;
+char choice = 'p';
+char Get_value = ' ';
+
 
 
 float Calibration_Process();
-
+void Read_Serial();
 void setup() {
   Serial.begin(9600);
   Serial.println("---------------------------------------------Starting----------------------------------------------------------");
   Serial.println("---------------------------------------Wait about 2 seconds----------------------------------------------------");
   LoadCell.begin();
   LoadCell.start(2000, true);
+
+
   Serial.print("Do you want to calibrate first? y | n : ");
   while (1) {
     if (Serial.available()) {
@@ -37,16 +46,47 @@ void setup() {
       }
     }
   }
+
   pinMode(Throttle_Pot, INPUT);
   // pinMode(ESC_OUT,OUTPUT);
   motor.attach(ESC_OUT, 1000, 2000);  // 1ms - 2ms pulse width out of 20ms cycle
+  Serial.println("Please, Enter how to change thrust p: Potentiometer, b:bluetooth, S:Serial Monitor");
+  while (true) {  // Choosing how to vary thrust
+    if (Serial.available()) {
+      choice = Serial.read();
+      Serial.println(choice);
+      break;
+    }
+  }
+  Throttle = analogRead(Throttle_Pot);
+  while (5 <= Throttle) {  //Saftey measure
+    Serial.println("-------------------- Please, Set throttle to Zero----------------------");
+    Throttle = analogRead(Throttle_Pot);
+  }
 }
 
 void loop() {
-  if (LoadCell.update())
+  if (LoadCell.update()) {
     Serial.println(LoadCell.getData());
-  Throttle = map(analogRead(Throttle_Pot), 0, 1023, 1000, 2000);
-  motor.writeMicroseconds(Throttle);
+  }
+  switch (choice) {
+    case 'b':
+      //Throttle = analogRead(Throttle_Pot);
+    case 'B':
+      // Throttle = analogRead(Throttle_Pot);
+      // break;
+    case 's':
+      Read_Serial();
+      break;
+    case 'S':
+      Read_Serial();
+      break;
+    default:
+    Throttle = map(analogRead(Throttle_Pot), 0, 1023, 0, 100);
+  }
+  int New_Throttle = map(Throttle, 0, 100, 1000, 2000);// Just for a garabage problem appeared so I made a new variable
+  Serial.println(New_Throttle);
+  motor.writeMicroseconds(New_Throttle);
 }
 
 float Calibration_Process() {
@@ -90,4 +130,22 @@ float Calibration_Process() {
   Serial.print(newCalibrationValue);
 
   return newCalibrationValue;
+}
+void Read_Serial() {
+  Get_value = Serial.read();
+  if ('v' == Get_value) {
+    Serial.println("Please, Enter a Value between 0 : 100");
+    while (true) {
+      if (Serial.available()) {
+        Throttle = Serial.parseInt();
+        Serial.print(Throttle);
+        Serial.println('%');
+        if (Throttle > 100) {
+          Serial.println("Please, Enter a valid Value between 0 :100");
+        } else {
+          break;
+        }
+      }
+    }
+  }
 }
