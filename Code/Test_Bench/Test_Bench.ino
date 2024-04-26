0bccb56cc43c00b11ca66ec24a0af04750d7723b
AIM75 <ahmedibrahimmohamed75@gmail.com>
2024-04-27 01:29:33 +0300

    [added] changing how you send the throttle value(bluetooth is not verified), rpm calculation(to be verified, simulation issues)

--- Code/Test_Bench/Test_Bench.ino
+++ Code/Test_Bench/Test_Bench.ino
@@ -3,27 +3,41 @@ Initial configuration must be done while connecting to the arduino
 */
 #include <HX711_ADC.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 #define HX_DOUT 4
 #define HX_SCK 5
 #define Throttle_Pot A1
 #define ESC_OUT 9
+#define Rx 10
+#define Tx 11
+#define IR 2
 
 HX711_ADC LoadCell(HX_DOUT, HX_SCK);
 Servo motor;
+SoftwareSerial Bluetooth(Rx, Tx);  // RX, TX
+
 
 float Calibration_Factor = 450;  //Can not be equal to zero Nan
 float Thrust = 0;
 int Throttle = 0;
 char choice = 'p';
 char Get_value = ' ';
-
+long long Calc_Time = 0;
+int counter;
+char Encoder_Value = 2;
+float rpm;
 
 
 float Calibration_Process();
 void Read_Serial();
+void Read_Bluetoooth();
+void Read_pot();
+void Saftey_measure();
+void RPM_Calculation();
 void setup() {
   Serial.begin(9600);
+  Bluetooth.begin(9600);
   Serial.println("---------------------------------------------Starting----------------------------------------------------------");
   Serial.println("---------------------------------------Wait about 2 seconds----------------------------------------------------");
   LoadCell.begin();
@@ -48,6 +62,7 @@ void setup() {
   }
 
   pinMode(Throttle_Pot, INPUT);
+  pinMode(IR, INPUT_PULLUP);
   // pinMode(ESC_OUT,OUTPUT);
   motor.attach(ESC_OUT, 1000, 2000);  // 1ms - 2ms pulse width out of 20ms cycle
   Serial.println("Please, Enter how to change thrust p: Potentiometer, b:bluetooth, S:Serial Monitor");
@@ -63,18 +78,19 @@ void setup() {
     Serial.println("-------------------- Please, Set throttle to Zero----------------------");
     Throttle = analogRead(Throttle_Pot);
   }
+  Calc_Time = millis();
+  attachInterrupt(digitalPinToInterrupt(IR), RPM_Calculation, RISING);
+   Serial.print("Throttle( % )           ");Serial.print("         Thrust( gm )           ");Serial.println("        RPM           ");
 }
 
 void loop() {
-  if (LoadCell.update()) {
-    Serial.println(LoadCell.getData());
-  }
   switch (choice) {
     case 'b':
-      //Throttle = analogRead(Throttle_Pot);
+      Read_Bluetoooth();
+      break;
     case 'B':
-      // Throttle = analogRead(Throttle_Pot);
-      // break;
+      Read_Bluetoooth();
+      break;
     case 's':
       Read_Serial();
       break;
@@ -82,11 +98,22 @@ void loop() {
       Read_Serial();
       break;
     default:
-    Throttle = map(analogRead(Throttle_Pot), 0, 1023, 0, 100);
+      Read_pot();
   }
-  int New_Throttle = map(Throttle, 0, 100, 1000, 2000);// Just for a garabage problem appeared so I made a new variable
-  Serial.println(New_Throttle);
+  int New_Throttle = map(Throttle, 0, 100, 1000, 2000);  // Just for a garabage problem appeared so I made a new variable
+  // Serial.println(New_Throttle);
   motor.writeMicroseconds(New_Throttle);
+  if (1000 < millis() - Calc_Time) {
+    rpm = (float)counter * 60 / Encoder_Value;
+    counter = 0;
+    Serial.println(rpm);
+    Calc_Time = millis();
+  }
+  if (LoadCell.update()) {
+    //Serial.prntln(LoadCell.getData());
+    Serial.print(Throttle);Serial.print("                                ");Serial.print(LoadCell.getData());Serial.print("                     ");Serial.println(rpm);
+  }
+  
 }
 
 float Calibration_Process() {
@@ -138,14 +165,87 @@ void Read_Serial() {
     while (true) {
       if (Serial.available()) {
         Throttle = Serial.parseInt();
-        Serial.print(Throttle);
-        Serial.println('%');
-        if (Throttle > 100) {
-          Serial.println("Please, Enter a valid Value between 0 :100");
+        if (301 == Throttle) {
+          choice = 'p';
+          Saftey_measure();
+        } else if (302 == Throttle) {
+          choice = 'b';
+          Saftey_measure();
         } else {
-          break;
+          Serial.print(Throttle);
+          Serial.println('%');
+          if (Throttle > 100) {
+            Serial.println("Please, Enter a valid Value between 0 :100");
+          } else {
+            break;
+          }
         }
       }
     }
   }
+}
+void Read_Bluetoooth() {
+  Get_value = Bluetooth.read();
+  if ('v' == Get_value) {
+    Bluetooth.println('b');
+    while (true) {
+      if (Bluetooth.available()) {
+        Throttle = Bluetooth.parseInt();
+        if (301 == Throttle) {
+          choice = 'p';
+          Saftey_measure();
+        } else if (302 == Throttle) {
+          choice = 's';
+          Saftey_measure();
+        } else {
+          Bluetooth.print(Throttle);
+          Bluetooth.println('%');
+          if (Throttle > 100) {
+            Bluetooth.println("Please, Enter a valid Value between 0 :100");
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  }
+}
+void Read_pot() {
+  char Chg = 0;
+  if (Serial.available()) {
+    Chg = Serial.read();
+    if ('s' == Chg) {
+      choice = Chg;
+      Saftey_measure();
+    } else if ('b' == Chg) {
+      choice = Chg;
+      Saftey_measure();
+    } else {
+      choice = 'p';
+    }
+
+  } else if (Bluetooth.available()) {
+    Chg = Bluetooth.read();
+    if ('s' == Chg) {
+      choice = Chg;
+      Saftey_measure();
+    } else if ('b' == Chg) {
+      choice = Chg;
+      Saftey_measure();
+    } else {
+      choice = 'p';
+    }
+
+  } else {
+    Throttle = map(analogRead(Throttle_Pot), 0, 1023, 0, 100);
+  }
+}
+void Saftey_measure() {
+  while (5 <= Throttle) {  //Saftey measure
+    Serial.println("-------------------- Please, Set throttle to Zero----------------------");
+    Throttle = map(analogRead(Throttle_Pot), 0, 1023, 0, 100);
+  }
+}
+void RPM_Calculation() {
+  counter = counter + 1;
 }
